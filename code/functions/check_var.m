6278a05bdce8f9dda33e442f5b02f27c74613925
e5k <seb@eskd.net>
2016-08-08 16:32:16 -1000

    GUI update
    Finished implementing input fields

--- code/functions/check_var.m
+++ code/functions/check_var.m
@@ -1,6 +1,7 @@
-function check_var(src, evt)
+function check_var(src, ~)
 jEdit = findjobj(src);
 
+%% Retrieve GUI data
 part = guidata(src);
 
 %% Project pannels
@@ -15,17 +16,17 @@ elseif strcmp(src.Tag, 'vent_lat')
 elseif strcmp(src.Tag, 'vent_lon')  
     tmp = str2double(src.String);
     err = 'Check the vent longitude';
-    if isnan(tmp) || tmp>180 || tmp<-180 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,1); part.vent.lon = tmp; end
+    if isnan(tmp) || tmp>180 || tmp<-180 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.vent.lon = tmp; end
     
 elseif strcmp(src.Tag, 'vent_alt')
     tmp = str2double(src.String);
     err = 'Check the vent altitude';
-    if isnan(tmp) || tmp<0 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,1); part.vent.alt = tmp; end
+    if isnan(tmp) || tmp<0 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.vent.alt = tmp; end
     
 elseif strcmp(src.Tag, 'date')
     tmp = src.String;
     err = 'Enter a valid date (see datestr in Matlab help)';
-    if isdatetime(tmp) ; change_frame(jEdit,src,1,' '); part.date = tmp; else change_frame(jEdit,src,0,err); end
+    if ~isempty(regexp(tmp,'\d\d-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-\d\d\d\d \d\d:\d\d:\d\d', 'once')) ; change_frame(jEdit,src,1,' '); part.date = tmp; else change_frame(jEdit,src,0,err); end
     
 elseif strcmp(src.Tag, 'atm')   
     tmp = src.String;
@@ -36,6 +37,108 @@ elseif strcmp(src.Tag, 'dem')
     tmp = src.String;
     err = 'The specified DEM file does not exist';
     if exist(tmp, 'file') ; change_frame(jEdit,src,1,' '); part.path.dem = tmp; else change_frame(jEdit,src,0,err); end
+
+    
+%% Part pannels    
+elseif strcmp(src.Tag, 'part_name')
+    tmp = src.String;
+    err = 'Enter a particle name';
+    if isempty(tmp) ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.part.diam = tmp; end
+
+elseif strcmp(src.Tag, 'part_diam')
+    tmp = str2double(src.String);
+    err = 'Check the particle diameter';
+    if isnan(tmp) || tmp<0 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.part.diam = tmp; end
+
+elseif strcmp(src.Tag, 'part_dens')
+    tmp = str2double(src.String);
+    err = 'Check the particle density';
+    if isnan(tmp) || tmp<0 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.part.dens = tmp; end
+
+elseif strcmp(src.Tag, 'part_flat')
+    tmp = str2double(src.String);
+    err = 'Check the particle flatness';
+    if isnan(tmp) || tmp<0 || tmp>1; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.part.flat = tmp; end
+
+elseif strcmp(src.Tag, 'part_elon')
+    tmp = str2double(src.String);
+    err = 'Check the particle elongation';
+    if isnan(tmp) || tmp<0 || tmp>1; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.part.elon = tmp; end
+    
+
+%% Release pannels 
+elseif strcmp(src.Tag, 'rel_x')
+    tmp = str2double(src.String);
+    err = 'Check the x offset';
+    if isnan(tmp); change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.rel.x = tmp; end    
+    
+elseif strcmp(src.Tag, 'rel_y')
+    tmp = str2double(src.String);
+    err = 'Check the y offset';
+    if isnan(tmp); change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.rel.y = tmp; end 
+    
+elseif strcmp(src.Tag, 'rel_z')
+    tmp = str2double(src.String);
+    err = 'Check the release altitude';
+    if isnan(tmp) || tmp<0 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.rel.z = tmp; end    
+    
+elseif strcmp(src.Tag, 'rel_t')
+    tmp = str2double(src.String);
+    err = 'Check the time offset';
+    if isnan(tmp); change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.rel.t = tmp; end    
+    
+elseif strcmp(src.Tag, 'rel_vx')
+    tmp = str2double(src.String);
+    err = 'Check the x velocity';
+    if isnan(tmp); change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.rel.vx = tmp; end    
+    
+elseif strcmp(src.Tag, 'rel_vy')
+    tmp = str2double(src.String);
+    err = 'Check the y velocity';
+    if isnan(tmp); change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.rel.vy = tmp; end 
+    
+elseif strcmp(src.Tag, 'rel_vz')
+    tmp = str2double(src.String);
+    err = 'Check the z velocity';
+    if isnan(tmp); change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); if tmp == 0; tmp = 1e-4; end; part.rel.vz = tmp; end % Check if Vz = 0
+    
+    
+%% Advanced pannel
+elseif strcmp(src.Tag, 'adv_sol')
+    tmp = lower(src.String);
+    err = 'Solution is Euler, Analytical or RungeKutta';
+    if isempty(regexp(tmp, '(euler|analytical|rungekutta)', 'once')); change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.adv.solution = tmp; end 
+    
+elseif strcmp(src.Tag, 'adv_dt')
+    tmp = str2double(src.String);
+    err = 'Check the time step';
+    if isnan(tmp) || tmp<0 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.adv.dt = tmp; end    
+    
+elseif strcmp(src.Tag, 'adv_drag')
+    tmp = str2double(src.String);
+    err = 'Check the radius of reduced drag';
+    if isnan(tmp) || tmp<0 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.adv_drag = tmp; end    
+    
+elseif strcmp(src.Tag, 'adv_int')
+    tmp = lower(src.String);
+    err = 'Interpolation is None, Subset or Complete';
+    if isempty(regexp(tmp, '(none|subset|complete)', 'once')); change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.adv.interp = tmp; end 
+
+elseif strcmp(src.Tag, 'adv_meth')
+    tmp = lower(src.String);
+    err = 'Check interpolation method (interpn)';
+    if isempty(regexp(tmp, '(linear|nearest|pchip|cubic|spline)', 'once')); change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.adv.method = tmp; end 
+    
+elseif strcmp(src.Tag, 'adv_range')
+    tmp = str2double(src.String);
+    err = 'Check the interpolation range';
+    if isnan(tmp) || tmp<0 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.adv.range = tmp; end        
+    
+elseif strcmp(src.Tag, 'adv_skip')
+    tmp = str2double(src.String);
+    err = 'Check the time skip interval';
+    if isnan(tmp) || tmp<0 ; change_frame(jEdit,src,0,err); else change_frame(jEdit,src,1,' '); part.adv_skip = tmp; end    
+    
 end
 
 
--- main_GUI.m
+++ main_GUI.m
@@ -7,28 +7,31 @@ ypos = ceil((screensize(4)-sz(1))/2); % center the figure on the
 
 
 
-f = figure( 'Name', 'LagTrack', 'position',[xpos, ypos, sz(2), sz(1)], 'Toolbar','none' );
+f = figure( 'Name', 'LagTrack', 'position',[xpos, ypos, sz(2), sz(1)], 'Toolbar','none', 'Menubar', 'none', 'NumberTitle', 'off' );
 
 % Main container
-main    = uix.VBoxFlex( 'Parent', f, 'BackgroundColor', BGC, 'Padding', 5 );
+MAIN    = uix.VBoxFlex( 'Parent', f, 'BackgroundColor', BGC, 'Padding', 5 );
 
     % Top container
-    top     = uix.HBox( 'Parent', main , 'BackgroundColor', BGC);
+    top     = uix.HBox( 'Parent', MAIN , 'BackgroundColor', BGC);
         
+    TOP    = uix.HBoxFlex( 'Parent', top, 'BackgroundColor', BGC, 'Padding', 5 );
+    TOPL   = uix.BoxPanel( 'Parent', TOP, 'Title', 'Input', 'FontWeight', 'Bold', 'TitleColor', [.2 .2 .2], 'BackgroundColor', BGC,  'Padding', 5 );
+    TOPR   = uix.BoxPanel( 'Parent', TOP, 'Title', 'Display', 'FontWeight', 'Bold', 'TitleColor', [.2 .2 .2], 'BackgroundColor', BGC,  'Padding', 5 );
+    
         % Top left box
-        topL = uix.BoxPanel( 'Parent', top, 'Title', 'Input', 'FontWeight', 'Bold', 'TitleColor', [.2 .2 .2], 'BackgroundColor', BGC,  'Padding', 5 );
-        topL2= uix.VBox( 'Parent', topL);
-            
+        topL = uix.VBox( 'Parent', TOPL); 
             %Pannels
-            topLT = uix.TabPanel( 'Parent', topL2, 'Padding', 5 , 'BackgroundColor', BGC );
+            topLT = uix.TabPanel( 'Parent', topL, 'Padding', 5 , 'BackgroundColor', BGC );
                 topL_PROJ = uix.Panel( 'Parent', topLT );
                 topL_PART = uix.Panel( 'Parent', topLT );
-                topL_POS  = uix.Panel( 'Parent', topLT );
-                topL_VEL  = uix.Panel( 'Parent', topLT );
-                topL_OPT  = uix.Panel( 'Parent', topLT );
-                topLT.TabTitles = {'Project', 'Particle', 'Position', 'Velocity','Option'};
+                topL_REL  = uix.Panel( 'Parent', topLT );
+                %topL_VEL  = uix.Panel( 'Parent', topLT );
+                topL_ADV  = uix.Panel( 'Parent', topLT );
+                topLT.TabTitles = {'Project', 'Particle', 'Release', 'Advanced'};
                 
-                
+                    
+                    % Project
                     topL_proj = uix.Grid( 'Parent', topL_PROJ, 'Padding', 15, 'Spacing', 8, 'BackgroundColor', BGC );
                         topL_proj_nameL     = uicontrol( 'Parent', topL_proj, 'Style', 'Edit', 'String', 'Run name', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
                         topL_proj_latL      = uicontrol( 'Parent', topL_proj, 'Style', 'Edit', 'String', 'Vent latitude', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
@@ -59,6 +62,7 @@ main    = uix.VBoxFlex( 'Parent', f, 'BackgroundColor', BGC, 'Padding', 5 );
 
                         set( topL_proj,  'Heights', [35 35 35 35 35 0 35 35], 'Widths', [120, -1 40] );
                     
+                    % Particle  
                     topLT.Selection = 2;
                     topL_part = uix.Grid( 'Parent', topL_PART, 'Padding', 15, 'Spacing', 8, 'BackgroundColor', BGC );
                         topL_part_nameL     = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'String', 'Particle name', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
@@ -68,54 +72,91 @@ main    = uix.VBoxFlex( 'Parent', f, 'BackgroundColor', BGC, 'Padding', 5 );
                         topL_part_flatL     = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'String', 'Flatness', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
                         topL_part_elonL     = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'String', 'Elongation', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
 
-                        topL_part_name      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Particle name', 'Tag', 'name', 'callback', @check_var);
+                        topL_part_name      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Particle name', 'Tag', 'part_name', 'callback', @check_var);
                         uix.Empty( 'Parent', topL_part );
-                        topL_part_diam      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Particle diameter (mm)', 'Tag', 'name', 'callback', @check_var);
-                        topL_part_dens      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Density (kg/m^3)', 'Tag', 'name', 'callback', @check_var);
-                        topL_part_flat      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Flatness (i.e. ratio of small and intermediate axes)', 'Tag', 'name', 'callback', @check_var);
-                        topL_part_elon      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Elongation (i.e. ration of intermediate and large axes)', 'Tag', 'name', 'callback', @check_var);
+                        topL_part_diam      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Particle diameter (mm)', 'Tag', 'part_diam', 'String', '0.5', 'callback', @check_var);
+                        topL_part_dens      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Density (kg/m^3)', 'Tag', 'part_dens', 'String', '1000', 'callback', @check_var);
+                        topL_part_flat      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Flatness (i.e. ratio of small and intermediate axes)', 'Tag', 'part_flat', 'String', '0.7', 'callback', @check_var);
+                        topL_part_elon      = uicontrol( 'Parent', topL_part, 'Style', 'Edit', 'Tooltip', 'Elongation (i.e. ration of intermediate and large axes)', 'Tag', 'part_elon', 'String', '0.7', 'callback', @check_var);
                         
-                        set( topL_part,  'Heights', [35 0 35 35 35 35], 'Widths', [120, -1
-                            
-                        ] );
+                        set( topL_part,  'Heights', [35 0 35 35 35 35], 'Widths', [120, -1] );
+                    
+                    % Release    
+                    topLT.Selection = 3;
+                    topL_rel = uix.Grid( 'Parent', topL_REL, 'Padding', 15, 'Spacing', 8, 'BackgroundColor', BGC );                        
+                        topL_rel_xL         = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'String', 'X offset (m)', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame);                         
+                        topL_rel_yL         = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'String', 'Y offset (m)', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame);                         
+                        topL_rel_zL         = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'String', 'Altitude (m above vent)', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
+                        uix.Empty( 'Parent', topL_rel );
+                        topL_rel_tL         = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'String', 'Time offset (s)', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame);
+                        uix.Empty( 'Parent', topL_rel );                        
+                        topL_rel_vxL        = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'String', 'Initial X velocity (m/s)', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame);                         
+                        topL_rel_vyL        = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'String', 'Initial Y velocity (m/s)', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame);                         
+                        topL_rel_vzL        = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'String', 'Initial Z velocity (m/s)', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
+                                               
+                        topL_rel_x          = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'Tooltip', sprintf('X offset relative to the vent (m)\nPositive towards E, negative towards W'), 'Tag', 'rel_x', 'String', '0', 'callback', @check_var);                     
+                        topL_rel_y          = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'Tooltip', sprintf('Y offset relative to the vent (m)\nPositive towards N, negative towards S'), 'Tag', 'rel_y', 'String', '0', 'callback', @check_var);                     
+                        topL_rel_z          = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'Tooltip', 'Altitude above vent (m)', 'Tag', 'rel_z', 'String', '0', 'callback', @check_var);  
+                        uix.Empty( 'Parent', topL_rel );
+                        topL_rel_t          = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'Tooltip', sprintf('Time offset relative to the eruption date (sec)\nPositive in future, negative in past'), 'Tag', 'rel_t', 'String', '0', 'callback', @check_var);
+                        uix.Empty( 'Parent', topL_rel );                        
+                        topL_rel_vx         = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'Tooltip', sprintf('Initial velocity in X direction (m/s)\nPositive towards E, negative towards W'), 'Tag', 'rel_vx', 'String', '0', 'callback', @check_var);                     
+                        topL_rel_vy         = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'Tooltip', sprintf('Initial velocity in Y direction (m/s)\nPositive towards N, negative towards S'), 'Tag', 'rel_vy', 'String', '0', 'callback', @check_var);                        
+                        topL_rel_vz         = uicontrol( 'Parent', topL_rel, 'Style', 'Edit', 'Tooltip', sprintf('Initial velocity in Z direction (m/s)\nPositive upwards, negative downwards'), 'Tag', 'rel_vz', 'String', '0', 'callback', @check_var);
+                       
+                        set( topL_rel,  'Heights', [35 35 35 0 35 0 35 35 35], 'Widths', [120, -1] );
+                
+                    %topL_vel = uix.Grid( 'Parent', topL_VEL, 'Padding', 15, 'Spacing', 8, 'BackgroundColor', BGC );
+                
+                    % Advanced 
+                    topLT.Selection = 4;
+                    topL_adv = uix.Grid( 'Parent', topL_ADV, 'Padding', 15, 'Spacing', 8, 'BackgroundColor', BGC );
+                        topL_adv_solL       = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Solution', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
+                        uix.Empty( 'Parent', topL_adv );
+                        topL_adv_dTL        = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Time step (s)', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
+                        topL_adv_dragL      = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Reduced drag (m)', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
+                        uix.Empty( 'Parent', topL_adv );
+                        topL_adv_interL     = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Interpolation', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
+                        topL_adv_methodL    = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Method', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
+                        topL_adv_rangeL     = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Range', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
+                        topL_adv_skipL      = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Interpolation', 'BackgroundColor', BGC, 'Enable', 'off', 'HorizontalAlign', 'Left', 'CreateFcn', @remove_frame); 
                         
                         
-                
-                
-                    topL_pos = uix.Grid( 'Parent', topL_POS, 'Padding', 15, 'Spacing', 8, 'BackgroundColor', BGC );
-                
-                
-                    topL_vel = uix.Grid( 'Parent', topL_VEL, 'Padding', 15, 'Spacing', 8, 'BackgroundColor', BGC );
-                
-                
-                    topL_opt = uix.Grid( 'Parent', topL_OPT, 'Padding', 15, 'Spacing', 8, 'BackgroundColor', BGC );
-                
-                
-                
-
-                
+                        topL_adv_sol        = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Euler', 'Tooltip', sprintf('Solution. Enter Euler, Analytical or RungeKutta'), 'Tag', 'adv_sol', 'callback', @check_var);
+                        uix.Empty( 'Parent', topL_adv );
+                        topL_adv_dT         = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', '0.1', 'Tooltip', sprintf('Time step between advancing states (s)'), 'Tag', 'adv_dt', 'callback', @check_var);
+                        topL_adv_drag       = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', '0', 'Tooltip', sprintf('Radius of reduced drag (m above vent)'), 'Tag', 'adv_drag', 'callback', @check_var); 
+                        uix.Empty( 'Parent', topL_adv );
+                        topL_adv_inter      = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Subset', 'Tooltip', sprintf('Interpolation of atmospheric data, either None, Subset or Complete\n-None: No interpolation\n-Subset: Only specified indices around current points are interpolated\n-Complete: Then entire atmospheric dataset is interpolated at each time step'), 'Tag', 'adv_int','callback', @check_var);
+                        topL_adv_method     = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', 'Linear', 'Tooltip', sprintf('Interpolation method, either linear, nearest, pchip, cubic or spline\nSee the Matlab documentation for interpn for more detail'), 'Tag', 'adv_meth', 'callback', @check_var);
+                        topL_adv_range      = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', '1', 'Tooltip', sprintf('Range of neighbor indexes used for subset interpolation (higher: slower interpolation)'), 'Tag', 'adv_range', 'callback', @check_var);
+                        topL_adv_skip       = uicontrol( 'Parent', topL_adv, 'Style', 'Edit', 'String', '0', 'Tooltip', sprintf('Number of time steps to skip between two interpolations (higher: faster interpolation)'), 'Tag', 'adv_skip', 'callback', @check_var);
+                        
+                        set( topL_adv,  'Heights', [35 0 35 35 0 35 35 35 35], 'Widths', [120, -1] );
+                        
+                        
+                    topLT.Selection = 1;
+                    
             
                 
                 
             
-            topLB = uix.HBox('Parent', topL2, 'Padding', 5 , 'BackgroundColor', BGC);
-            %uix.HButtonBox( 'Parent', topL2, 'Padding', 5 , 'BackgroundColor', BGC   );
+            topLB = uix.HBox('Parent', topL, 'Padding', 5 , 'BackgroundColor', BGC);
             errorE = uicontrol( 'Parent', topLB, 'Style', 'Edit', 'String', '  ', 'HorizontalAlign', 'left', 'Tag','Errmsg');
             uicontrol( 'Parent', topLB, 'Style', 'Pushbutton', 'String', 'Run' );
             set( topLB, 'Widths', [-1 120], 'Spacing', 5 );
-            %set( topLB, 'ButtonSize', [100 45], 'Spacing', 5 );
         
-        set(topL2, 'Heights', [-1 45]);
+        set(topL, 'Heights', [-1 45]);
         
         % Top right box
-        topR = uix.BoxPanel( 'Parent', top, 'Title', 'Display', 'FontWeight', 'Bold', 'TitleColor', [.2 .2 .2], 'BackgroundColor', BGC,  'Padding', 5 );
+        %topR = uix.BoxPanel( 'Parent', TOPL, 'Title', 'Display', 'FontWeight', 'Bold', 'TitleColor', [.2 .2 .2], 'BackgroundColor', BGC,  'Padding', 5 );
 
-    set( top, 'Widths', [-1 -1], 'Spacing', 5 );
+    set( TOP, 'Widths', [-1 -1], 'Spacing', 5 );
 
     % Bottom container
-    bottom  = uix.BoxPanel( 'Parent', main, 'Title', 'Input', 'FontWeight', 'Bold', 'TitleColor', [.2 .2 .2], 'BackgroundColor', BGC,  'Padding', 5 );
+    bottom  = uix.BoxPanel( 'Parent', MAIN, 'Title', 'Input', 'FontWeight', 'Bold', 'TitleColor', [.2 .2 .2], 'BackgroundColor', BGC,  'Padding', 5 );
 
-set(main, 'Heights', [-1 200], 'Spacing', 5 );
+set(MAIN, 'Heights', [-1 200], 'Spacing', 5 );
 
 
 
